209a96ac30bbe92d3c3dddcc5c729a6af300986a
rafhaellemes <rafhael.lemes@yahoo.com>
2021-04-15 11:21:37 -0300

    1.0.0.7

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const app = express();
 // Serve only the static files form the dist directory
 app.use(express.static(__dirname + '/dist/notefix'));
 app.get('/*', function(req,res) {
-   res.sendFile(path.join(__dirname+'/dist/notefix/index.html'));
+   res.sendFile('index.html', {root: 'dist/notefix/'});
 });
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
